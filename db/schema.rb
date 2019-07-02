01838e7dcd58648b842fc80766693475f993c9e3
Joel Velis <joelvelis94@gmail.com>
2019-07-02 15:40:47 -0400

    change database from sqlite to postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_30_014542) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.date "date"
